0a21db87a74af8b48f4f85ed07852b2d8d9b29d5
samcesa45 <samcesa45@gmail.com>
2021-04-01 10:18:01 -0700

    amplify.yml

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1>Hello from v2</h1>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
+			<header>
+				<AmplifySignOut />
 			</header>
-			<AmplifySignOut />
+			<h1>Hello from v2</h1>
 		</div>
 	);
 }
